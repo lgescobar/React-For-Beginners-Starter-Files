86923ff896a149a257aa24dcc2a6a3bf76bf1e4d
Luis Antonio García Escobar <l.garcia@ko-web.net>
2018-12-28 13:57:03 +0100

    Add convenience method to add sample fishes to speed up testing

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header'
 import Order from './Order'
 import Inventory from './Inventory'
+import sampleFishes from '../sample-fishes'
 
 class App extends React.Component {
   state = {
@@ -29,6 +30,10 @@ class App extends React.Component {
     this.setState({fishes});
   };
 
+  loadSampleFishes = () => {
+    this.setState({fishes: sampleFishes});
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -36,7 +41,7 @@ class App extends React.Component {
           <Header tagline="Louis is on fire"/>
         </div>
         <Order/>
-        <Inventory addFish={this.addFish}/>
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
       </div>
     )
   }
